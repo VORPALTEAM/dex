4882a7ece11b8c8d066d723e324225dbf36edb57
Yuriy <berlandspw@gmail.com>
2023-10-03 18:27:14 +0500

    url link test

--- src/views/Home/components/Hero.tsx
+++ src/views/Home/components/Hero.tsx
@@ -215,7 +215,7 @@ const Hero = () => {
            <BorderedHeading />    
           <HeroBtnFlex className={account ? "centered" : ""}>
             {!account && <HeroConnectButton mr="42px" />}
-            <Link to={{ pathname: "https://sale.vorpal.finance/" }}>
+            <Link to={{ pathname: "https://sale.vorpal.finance/" }} target="_blank">
               <HeroAddButton variant={!account ? 'secondary' : 'primary'}>
                 {t('Invest now!')}
               </HeroAddButton>
